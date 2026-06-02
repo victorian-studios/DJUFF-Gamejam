982fac6df43a82f02cb17b61284805adba6ee5ed
AnaVSSCaldeira <anavsscaldeira@gmail.com>
2026-06-02 19:59:35 -0300

    small change

--- djuff/Menu/Scripts/menu.gd
+++ djuff/Menu/Scripts/menu.gd
@@ -5,7 +5,7 @@ func _ready():
     $Screens/Credits.visible = false
 
 func _on_play_pressed():
-    pass # Replace with function body.
+    get_tree().change_scene_to_file("res://Game/Scenes/MainGame.tscn")
 
 func _on_how_to_play_pressed():
     $Screens/HowToPlay.visible = !($Screens/HowToPlay.visible)
